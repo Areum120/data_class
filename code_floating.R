4dd5be8ffda863a470e2fa67e51a7f18140e8c32
Kyeongrok <oceanfog1@gmail.com>
2020-09-13 23:39:47 +0900

    fileEncoding

--- code_floating.R
+++ code_floating.R
@@ -1,4 +1,11 @@
 
 getwd()
-df <- read.csv(file="Floating_Population_2008.csv", sep=",", header = TRUE, encoding="UTF-8")
+df <- read.csv(file="Floating_Population_2008.csv",
+               header = T,
+               fileEncoding = 'utf-8',
+               # encoding="utf-8"
+               )
 df
+
+# 구별, 연령대별로 groupby sum 60대와 70대 유동인구 합계를 구한 셀을 만듬
+# 노인인구 60~70 top 10
